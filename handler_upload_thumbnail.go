a2bd2041dacd5fb7fcd46cd6230c4dfef4b5c290
horribleinternet <cyberblah@gmail.com>
2025-08-28 23:55:12 +0900

    store thumbnails in the database

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +30,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File header missing", err)
+		return
+	}
+
+	mediaType := header.Header.Get("Content-Type")
+	if len(mediaType) == 0 {
+		respondWithError(w, http.StatusBadRequest, "Missing media type", err)
+		return
+	}
+
+	bytes, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unknown video id", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Access denied", err)
+		return
+	}
+
+	newUrl := "data:" + mediaType + ";base64," + base64.StdEncoding.EncodeToString(bytes)
+	video.ThumbnailURL = &newUrl
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't update database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
